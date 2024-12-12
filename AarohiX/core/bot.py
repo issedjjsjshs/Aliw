b98f7ef04d9beb01bd0647820d32e9afdb66e084
issedjjsjshs <hsjehejehei@gmail.com>
2024-12-12 03:43:49 +0300

    Update bot.py

--- AarohiX/core/bot.py
+++ AarohiX/core/bot.py
@@ -35,19 +35,19 @@ class Dil(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-            exit()
+            
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
-            exit()
+            
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
-            exit()
+            
         LOGGER(__name__).info(f"تم تشغيل البوت بنجاح  {self.name}")
 
     async def stop(self):
